69061873bbe2e85a6f41521805eb3753b5276887
Mehmet Küçükdemir <mehmetmac1@gmail.com>
2023-07-24 09:04:30 +0300

    Rule of 3 has been noted as a comment

--- Classes Objects/Integer.h
+++ Classes Objects/Integer.h
@@ -1,4 +1,12 @@
 #pragma once
+/* *
+* Rule of 3
+* All should be defined if a user implements any of them
+* - Destructor [to free the resources]
+* - Copy constructor [to perform a deep copy]
+* - Copy assignment operator [to perform a deep copy]
+* If not, then memory leak or shallow copy can be occured
+*/
 class Integer {
 	int *pInt;
 public:
